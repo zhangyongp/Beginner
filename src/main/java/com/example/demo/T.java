02d44145bf768ed3d5f69072ecb54b9acfa2727f
zhangyongp <201215210@qq.com>
2021-11-24 17:53:01 +0800

    更改

---
+++ src/main/java/com/example/demo/T.java
@@ -0,0 +1,27 @@
+package com.example.demo;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
+
+import static java.lang.Thread.sleep;
+
+public class T {
+
+    public static void main(String[] args) throws InterruptedException {//创建使用固定线程数的线程池
+        ExecutorService es2 = Executors.newFixedThreadPool(5);
+
+
+        for (int i = 0; i < 20; i++) {
+            System.out.println("这是"+i);
+            int a = i;
+            es2.submit(new Runnable() {
+                @Override
+                public void run() {
+                    System.out.println(Thread.currentThread().getName() + "正在执行任务"+ a);
+                }
+            });
+        }
+
+        System.out.println("执行完毕");
+}}
