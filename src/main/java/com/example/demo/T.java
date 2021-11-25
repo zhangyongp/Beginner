a81cd17896b766ef3ab5e3a759a5d653946e7624
zhangyongp <94944668+zhangyongp@users.noreply.github.com>
2021-11-25 09:38:34 +0800

    Update T.java

--- src/main/java/com/example/demo/T.java
+++ src/main/java/com/example/demo/T.java
@@ -11,6 +11,8 @@ public class T {
     public static void main(String[] args) throws InterruptedException {//创建使用固定线程数的线程池
         ExecutorService es2 = Executors.newFixedThreadPool(5);
         System.out.println("122");
+        System.out.println("这是在做测试");
+        
 
         for (int i = 0; i < 20; i++) {
             System.out.println("这是"+i);
